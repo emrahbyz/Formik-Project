e15fa4266a3a53c68b0de6270eb012532d155a6b
emrahbyz <146847947+emrahbyz@users.noreply.github.com>
2024-02-13 18:48:59 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import GeneralForm from "./Components/GeneralForm";
+import PortalForm from "./Components/PortalForm";
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<GeneralForm />} />
+        <Route path="/portal" element={<PortalForm />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
